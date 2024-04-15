e7274f01262b514c5281e121f9411a1bcff321a9
JohnZhang888 <98006880+JohnZhang888@users.noreply.github.com>
2024-04-15 23:06:14 +0800

    Theme & Logo update

--- script/main.js
+++ script/main.js
@@ -1,11 +1,31 @@
 //const changeTheme = document.querySelector('#changeTheme')
 const html = document.querySelector('html')
-function lightTheme() {
+const mediaQueryListDark = window.matchMedia('(prefers-color-scheme: dark)')
+let theme
+if (mediaQueryListDark.matches) {
+    theme = 'dark'
+    html.setAttribute('data-bs-theme', 'dark')
+}
+else {
+    theme = 'light'
+    html.setAttribute('data-bs-theme', 'light')
+}
+function changeTheme() {
+    if (theme == 'light') {
+        theme = 'dark'
+        html.setAttribute('data-bs-theme', 'dark')
+    }
+    else if (theme == 'dark') {
+        theme = 'light'
+        html.setAttribute('data-bs-theme', 'light')   
+    }
+}
+/*function lightTheme() {
     html.setAttribute('data-bs-theme', 'light')
 }
 function darkTheme() {
     html.setAttribute('data-bs-theme', 'dark')
-}
+}*/
 
 /*changeTheme.addEventListener("change", (event) => {
     console.log(changeTheme.value)
